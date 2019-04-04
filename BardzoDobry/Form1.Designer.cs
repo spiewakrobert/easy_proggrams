930d4801fe25c305cdb1e3f95db0b05e5db14e94
Śpiewak Robert <rs051487@student.ath.edu.pl>
2019-04-05 01:55:28 +0200

    first test

---
+++ BardzoDobry/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BardzoDobry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 210);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(793, 225);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(49, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Iteracje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(817, 447);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BardzoDobry/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BardzoDobry
+{
+    public partial class Form1 : Form
+    {
+        int[,] PlikTrasy;
+        int[] trasa;
+        int DlugoTrasy;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PlikTrasy = new int[49, 49];
+            trasa = new int[49];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            z_pliku();
+            //for(int i=0; i<49;i++)
+            //{
+            //    for(int j=0; j<49; j++)
+            //    {
+            //        richTextBox1.Text += PlikTrasy[i, j]+ "  ";
+
+            //    }
+            //    richTextBox1.Text += Environment.NewLine;
+            //}
+            for(int i=0; i<49; i++)
+            {
+                trasa[i] = i;
+
+
+            }
+
+            DlugoTrasy =licz(trasa);
+            richTextBox1.Text = "Pierwszy wynik: "+Convert.ToString(DlugoTrasy)+Environment.NewLine;
+
+            int iteracje = Convert.ToInt32(textBox1.Text);
+
+            for(int i=0; i<iteracje; i++)
+            {
+                mutuj();
+
+
+            }
+            DlugoTrasy = licz(trasa);
+            richTextBox1.Text +="Ostateczny wynik: " +Convert.ToString(DlugoTrasy);
+
+        }
+
+        private int licz(int[] trasa2)
+        {
+            int trasa_pomocnicza=0;
+            for(int i=0; i<48; i++)
+            {
+                trasa_pomocnicza += PlikTrasy[trasa2[i], trasa2[i + 1]];
+
+
+            }
+            trasa_pomocnicza += PlikTrasy[trasa2[48], trasa2[0]];
+
+            //richTextBox1.Text = Convert.ToString(trasa_pomocnicza);
+            return trasa_pomocnicza;
+        }
+
+        private void mutuj()
+        {
+            Random r = new Random();
+            int[] pomocnicza_trasa = new int[49];
+
+            for (int i = 0; i < 49; i++)
+                pomocnicza_trasa[i] = trasa[i];
+
+            
+            int I_miasto = r.Next(1, 48);
+            int II_miasto = r.Next(1, 48);
+            int pomocnicza = trasa[I_miasto];
+            pomocnicza_trasa[I_miasto] = pomocnicza_trasa[II_miasto];
+            pomocnicza_trasa[II_miasto] = pomocnicza;
+
+            if(licz(trasa) >licz(pomocnicza_trasa))
+            {
+                for (int i = 0; i < 49; i++)
+                    trasa[i] = pomocnicza_trasa[i];
+
+
+            }
+
+
+        }
+
+        private void z_pliku()
+        {
+
+
+
+            int id = 0;
+            using (StreamReader SR = new StreamReader("..\\..\\miasta.txt"))
+            {
+                string wiersz;
+                while ((wiersz = SR.ReadLine()) != null)
+                {
+                    string[] spliter = wiersz.Split(',');
+                    for (int i = 0; i < spliter.Length; i++)
+                    {
+                        int dlugosc = Convert.ToInt32(spliter[i]);
+                       PlikTrasy[id, i] = dlugosc;
+                    }
+                    id++;
+                }
+            }
+
+
+        }
+
+
+    }
+}
